967b32f2ddfe678ae3e09670e9aed83c8c4fff7d
jia0_nn <linao264590000@gmail.com>
2020-08-23 15:48:21 +0800

    firestore func

--- src/app/components/note-cloud-store/note-cloud-store.component.ts
+++ src/app/components/note-cloud-store/note-cloud-store.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Note } from 'src/app/model/note';
+import { NoteListService } from 'src/app/notes/note-list.service';
+import { NgForm } from '@angular/forms';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-note-cloud-store',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoteCloudStoreComponent implements OnInit {
 
-  constructor() { }
+  notes: Note[] = [];
+  showRaw: boolean = true;
+  constructor(private noteService: NoteListService) { }
+  ngOnInit() {
+    this.getNotes();
+  }
+  getNotes() {
+    this.notes.length = 0;
+    this.noteService.getNotesFromCloudFirestore()
+      .snapshotChanges()
+      .forEach(actions => {
+        console.log('snap cloud', actions)
+        this.notes = []
+        actions.forEach(action => {
+          let item = action.payload.doc;
+          var data = item.data();
+          data['$key'] = item.id;
+          this.notes.push(data as Note);
+        })
+      });
+  }
 
-  ngOnInit(): void {
+  deleteFirstNote() {
+    console.log(this.notes[0].$key)
+    this.noteService.deleteNoteOnCloudFirestore(this.notes[0].$key);
+  }
+
+  addNote(form: NgForm) {
+    this.noteService.addNoteToCloudFirestore(form.value);
   }
 
 }
--- src/app/notes/note-list.service.ts
+++ src/app/notes/note-list.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore'
 import { Note } from '../model/note';
 
 
@@ -8,8 +9,10 @@ import { Note } from '../model/note';
 })
 export class NoteListService {
   private noteCollection: AngularFireList<any>;
-  constructor(private firebase: AngularFireDatabase) {
+  private noteCloudFirestoreCollection: AngularFirestoreCollection<any>;
+  constructor(private firebase: AngularFireDatabase, private firestore: AngularFirestore) {
     this.noteCollection = this.firebase.list("realtime-notes")
+    this.noteCloudFirestoreCollection = this.firestore.collection('cloud-firestore-notes');
   }
 
   getNotes = () => this.noteCollection;
@@ -20,5 +23,15 @@ export class NoteListService {
     let $key = note.$key;
     delete note.$key;
     this.noteCollection.update($key, note);
+  }
+
+
+  getNotesFromCloudFirestore = () => this.noteCloudFirestoreCollection;
+  addNoteToCloudFirestore = (note: Note) => {
+    note.isUrgent = !!note.isUrgent;
+    this.noteCloudFirestoreCollection.add(note);
+  }
+  deleteNoteOnCloudFirestore = ($key: string) => {
+    this.noteCloudFirestoreCollection.doc($key).delete();
   }
 }
